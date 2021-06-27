1cfdad1ccd7429fa444936c92dd725e519b3caef
deroude <rvgabriel@yahoo.com>
2021-06-27 17:40:26 +0300

    compilation

--- src/ModuleDiscovery.spec.ts
+++ src/ModuleDiscovery.spec.ts
@@ -1,4 +1,4 @@
-import { load } from './index';
+import { load } from './ModuleDiscovery';
 
 test('loads test module', () => {
     jest.mock('./package.json', () => ({ dependencies: { "dep": "1.0" } }));
